b8c2208ee7b48c67a1cb857ccf16ee5ad5d5ae92
_ltnhuwaaa <pkhanh.dev@gmail.com>
2025-07-23 14:14:20 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os,platform
+
+try:
+    import requests
+    import time
+    import os 
+    from art import *
+    from colorama import Fore
+    import time
+    import json
+    import random
+    from time import sleep
+    import sys
+    import cloudscraper
+    from tabulate import tabulate
+except:
+    os.system("pip install requests")
+    os.system("pip install requests")
+    os.system("pip install tabulate")
+    os.system("pip install art")
+    os.system("pip install colorama")
+    os.system("pip install cloudscraper")
+    import requests
+    
